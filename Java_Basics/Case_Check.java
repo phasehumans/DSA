f8adc4370a2f754c3203bf800c325dbaf6289528
chaitanya <dev.chaitanyasonawane@gmail.com>
2025-08-13 23:34:41 +0530

    conditionals & loops

---
+++ Java_Basics/Case_Check.java
@@ -0,0 +1,7 @@
+package Java_Basics;
+
+public class Case_Check {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java_Basics/Condition.java
@@ -0,0 +1,25 @@
+package Java_Basics;
+
+public class Condition {
+    public static void main(String[] args) {
+
+        /*
+        * if
+        * elseif ---> multiple if statements
+        * else
+        * */
+
+        int age= 20;
+        if (age >= 18){
+            System.out.println("Can vote");
+        }
+        else{
+            System.out.println("Can not vote");
+        }
+
+
+
+
+
+    }
+}
